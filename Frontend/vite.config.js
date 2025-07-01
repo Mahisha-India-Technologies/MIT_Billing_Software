fa0b7566441b35e8a40b77ba7d8ecfc818ce971d
Mahisha-India-Technologies <mahishaindiatechnologies@gmail.com>
2025-07-01 07:14:11 +0530

    Modified frontend and backend with extended functionality

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,17 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'public/_redirects', // 👈 copy _redirects from public/
-          dest: '.'                // 👈 put it into dist/
-        }
-      ]
-    })
-  ]
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    // 👇 This is required for development only
+    historyApiFallback: true,
+  },
 });
