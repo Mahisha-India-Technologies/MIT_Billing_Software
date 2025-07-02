9935fb7be50f55ae29343889931df9a1689549cd
Mahisha-India-Technologies <mahishaindiatechnologies@gmail.com>
2025-07-02 17:18:12 +0530

    Modified image upload url

--- Backend/src/routes/products.js
+++ Backend/src/routes/products.js
@@ -41,11 +41,10 @@ router.use((req, res, next) => {
 router.post("/upload", upload.single("image"), (req, res) => {
   if (!req.file) {
     return res.status(400).json({ error: "No image uploaded" });
-
-    const backendDomain = "https://your-backend-domain.com";
   }
-
+  const backendDomain = "https://mit-billing-software.onrender.com";
   const imagePath = `${backendDomain}/images/products/${req.file.filename}`;
+
   // const imagePath = `images/products/${req.file.filename}`;
   res.status(200).json({ imagePath });
 });
